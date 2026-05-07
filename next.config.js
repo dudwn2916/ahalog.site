2f880dfecf20a340a7f52863cf504c4e6aff5893
dudwn2916 <dudwn2916@gmail.com>
2026-05-07 22:53:29 +0900

    a

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    // 빌드 시 ESLint 오류 무시 (개발 중 임시 설정)
-    ignoreDuringBuilds: true,
-  },
+  staticPageGenerationTimeout: 120,
 }
-
 module.exports = nextConfig
